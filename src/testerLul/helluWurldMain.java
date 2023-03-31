d61cc6e47a4e6165c2a7b5357463d9f024097751
r22019742 <r22019742@A-F001711.salsa.univ-amu.fr>
2023-03-31 16:22:26 +0200

    commit tester si ca marche je me suce

---
+++ src/testerLul/helluWurldMain.java
@@ -0,0 +1,11 @@
+package testerLul;
+
+public class helluWurldMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Worlder!") ;
+
+	}
+
+}
